b5504e12bcbe89d8e2a7263d1c56a836d09eefbb
Jncl <jncl@mac.com>
2014-04-24 00:20:20 +0100

    Simplify exception handling Flatten data structures before inserting into db

--- outputs/database.py
+++ outputs/database.py
@@ -2,6 +2,7 @@ import output
 import os
 import datetime
 import sqlite3
+import json
 
 # add logging support
 import logging
@@ -18,50 +19,42 @@ class Database(output.Output):
         dbName = os.path.join(data["dbPath"], 'airpi.db')
         try:
             conn = sqlite3.connect(dbName)
-            try:
-                # create table
-                conn.execute("CREATE TABLE airpi (timestamp, sensor_data, gps_data)")
-                # save changes
-                conn.commit()
-            except sqlite3.IntegrityError:
-                pass
-            except Exception as e:
-                log.exception("Database create Exception {0}".format(e))
-                raise e
-        except Exception as e:
-            log.exception("Database connect Exception {0}".format(e))
-            raise e
-        else:
+             # create table
+            conn.execute("CREATE TABLE airpi (timestamp, sensor_data, gps_data)")
+            # save changes
+            conn.commit()
             # close the connection
             conn.close()
+        except sqlite3.IntegrityError:
+            pass
+        except Exception as e:
+            log.exception("Database create Exception {0}: {1}".format(e, dbName))
+            raise e
 
     def outputData(self, dataPoints):
         global dbName
-        sData = []
-        lData = None
+        arr = []
+        sData = lData = None
 
         for i in dataPoints:
             # handle GPS data
             if i["name"] == "Location":
-                lData = {"disposition": i["disposition"], "name": i["location"], "exposure": i["exposure"], "ele": i["altitude"], "lat": i["latitude"], "lon": i["longitude"], "sensor": i["sensor"]}
+                lData = json.dumps({"disposition": i["disposition"], "name": i["location"], "exposure": i["exposure"], "ele": i["altitude"], "lat": i["latitude"], "lon": i["longitude"], "sensor": i["sensor"]})
             else:
-                sData.append({"name": i["name"], "value": i["value"], "unit": i["unit"], "symbol": i["symbol"], "sensor": i["sensor"]})
+                arr.append({"name": i["name"], "value": i["value"], "unit": i["unit"], "symbol": i["symbol"], "sensor": i["sensor"]})
 
-        log.debug("Database output: [{0}]".format(sData, lData))
+        sData = json.dumps(arr)
+        log.debug("Database input: [{0},{1}]".format(sData, lData))
         try:
             conn = sqlite3.connect(dbName)
-            try:
-                # add row to table
-                conn.execute("insert into airpi values (?, ?, ?)", (str(datetime.datetime.now()), sData, lData))
-                # save changes
-                conn.commit()
-            except Exception as e:
-                log.exception("Database insert Exception {0}".format(e))
-                raise e
-        except Exception as e:
-            log.exception("Database connect Exception: {0}".format(e))
-            raise e
-        else:
+            # add row to table
+            conn.execute("insert into airpi values (?, ?, ?)", (str(datetime.datetime.now()), sData, lData))
+            # save changes
+            conn.commit()
             # close the connection
             conn.close()
+        except Exception as e:
+            log.exception("Database insert Exception: {0}".format(e))
+            raise e
+        else:
             return True
