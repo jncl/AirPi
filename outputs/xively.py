f14f5d207846d6ae09439ad7135fa7ac3c495703
Jncl <jncl@mac.com>
2014-04-23 17:54:30 +0100

    Log info and add timeout

--- outputs/xively.py
+++ outputs/xively.py
@@ -1,13 +1,18 @@
 import output
 import requests
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 class Xively(output.Output):
     requiredData = ["APIKey", "FeedID"]
     optionalData = []
+
     def __init__(self, data):
         self.APIKey = data["APIKey"]
         self.FeedID = data["FeedID"]
+
     def outputData(self, dataPoints):
         arr = []
         a = l = None
@@ -19,10 +24,10 @@ class Xively(output.Output):
                 else:
                     arr.append({"id": i["name"], "current_value": i["value"]})
             a = json.dumps({"version": "1.0.0", "datastreams": arr, "location": l})
-            # print("Xively output: [%s]" % (a,))
-            z = requests.put("https://api.xively.com/v2/feeds/" + self.FeedID + ".json", headers = {"X-ApiKey":self.APIKey}, data = a)
+            log.debug("Xively output: [%s]" % (a,))
+            z = requests.put("https://api.xively.com/v2/feeds/" + self.FeedID + ".json", headers = {"X-ApiKey":self.APIKey}, data = a, timeout = 5.0)
             if z.text != "":
-                print("Xively Error: %s" % z.text)
+                log.error("Xively Error: %s" % z.text)
                 return False
             return True
         except Exception as e:
