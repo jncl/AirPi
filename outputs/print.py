649a4a1375d99c5c9b2ff6e14d1cb840634aa225
Jncl <jncl@mac.com>
2014-04-24 18:04:28 +0100

    Bugfix for print plugin

--- outputs/print.py
+++ outputs/print.py
@@ -4,14 +4,16 @@ import datetime
 class Print(output.Output):
     requiredData = []
     optionalData = []
+
     def __init__(self, data):
         pass
+
     def outputData(self, dataPoints):
-        print ""
-        print "Time: " + str(datetime.datetime.now())
+        print("")
+        print("Time: {0}".format(str(datetime.datetime.now())))
         for i in dataPoints:
-            if i["name"] == "Location":
-                print("GPS info: %s" % (i,))
+            if i["type"] == "Location":
+                print("GPS info: {0}".format(i,))
             else:
-                print i["name"] + ": " + str(i["value"]) + " " + i["symbol"]
+                print("{0}: {1} {2}".format(i["type"], str(i["value"]), i["symbol"]))
         return True
