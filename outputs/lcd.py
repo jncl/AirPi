1f1de02682e61138d84f65561a587d4924d574d9
Jncl <jncl@mac.com>
2014-06-16 18:22:49 +0100

    Use abbreviations for type

--- outputs/lcd.py
+++ outputs/lcd.py
@@ -1,6 +1,19 @@
 import output
 import os
 import lcddriver
+from time import sleep
+
+abbr = {
+    "Temperature" : "Temp",
+    "Pressure" : "P",
+    "Relative_Humidity" : "RH",
+    "Light_Level" : "LL",
+    "Light_Level_Lux" : "LLl",
+    "Nitrogen_Dioxide" : "NO2",
+    "Carbon_Monoxide" : "CO",
+    "Volume" : "Vol"
+}
+
 
 # add logging support
 import logging
@@ -20,16 +33,15 @@ class Database(output.Output):
 
     def outputData(self, dataPoints):
         line = 1
+        self.lcd.clear(bl=1)
         for i in dataPoints:
             self.log.debug("type: {0}, value: {1:.2f}, symbol: {2}".format(i["type"][:1], i["value"], i["symbol"]))
-            self.log.debug("#1 line: {0}, rows: {1}".format(line, self.rows))
             # handle GPS data
             if i["type"] == "Location":
                 self.lcd.display_string("GPS: {0} {1} {2}".format(i["lat"], i["lon"], i["ele"]), line)
             else:
-                self.lcd.display_string("{0}: {1:.2f} {2}".format(i["type"][:1], i["value"], i["symbol"]), line)
+                self.lcd.display_string("{0}: {1:.2f} {2}".format(abbr[i["type"]], i["value"], i["symbol"]), line)
+            sleep(2)
             line += 1
-            self.log.debug("#2 line: {0}, {1}, {2}".format(line, type(line), type(self.rows)))
             if line > self.rows:
                 line = 1
-                self.log.debug("#3 line: {0}".format(line))
