1cc2a7f1ca1d6ea0809daa8c0f6b763393b790f2
Jncl <jncl@mac.com>
2014-06-16 15:19:01 +0100

    Add LCD output module

---
+++ outputs/lcd.py
@@ -0,0 +1,26 @@
+import output
+import os
+import lcddriver
+
+# add logging support
+import logging
+mod_log = logging.getLogger('airpi.lcd')
+
+class Database(output.Output):
+    requiredData = ["lcdAddr"]
+    optionalData = []
+    lcd = None
+
+    def __init__(self, data):
+        self.log = logging.getLogger('airpi.lcd')
+        self.lcdAddr = data["lcdAddr"]
+        self.lcd = lcddriver.lcd(self.lcdAddr)
+        self.lcd.clear(bl=0)
+
+    def outputData(self, dataPoints):
+        for i in dataPoints:
+            # handle GPS data
+            if i["type"] == "Location":
+                self.lcd.display_string("Lat: {0}, Lon: {1}, Elev: {2}".format(i["lat"], i["lon"], i["ele"]))
+            else:
+                self.lcd.display_string("{0}: {1}{2}".format(i["type"], i["value"], i["symbol"]))
