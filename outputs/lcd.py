43a6fffcf8e62f5c740f531bbaa26ee84fc8c3e8
Jncl <jncl@mac.com>
2014-06-16 17:31:30 +0100

    Add debug reformat GPS info

--- outputs/lcd.py
+++ outputs/lcd.py
@@ -21,9 +21,10 @@ class Database(output.Output):
     def outputData(self, dataPoints):
         line = 1
         for i in dataPoints:
+            self.log.debug("type: {0}, value: {1:.4f}, symbol: {2}, line: {3}".format(i["type"][:1], i["value"], i["symbol"], line))
             # handle GPS data
             if i["type"] == "Location":
-                self.lcd.display_string("Lat: {0}, Lon: {1}, Elev: {2}".format(i["lat"], i["lon"], i["ele"]), line)
+                self.lcd.display_string("GPS: {0} {1} {2}".format(i["lat"], i["lon"], i["ele"]), line)
             else:
                 self.lcd.display_string("{0}: {1:.4f} {2}".format(i["type"][:1], i["value"], i["symbol"]), line)
             line += 1
