f7e8301c064da67238d718fa07ee77affdbd939a
Jncl <jncl@mac.com>
2014-06-17 23:31:02 +0100

    Add signal handler to cleanup

--- airpi.py
+++ airpi.py
@@ -39,6 +39,28 @@ outputscfg  = os.path.join(cfgdir, 'outputs.cfg')
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) #Use BCM GPIO numbers.
 
+# placeholders for Plugin objects
+gpsPluginInstance = None
+lcdPluginInstance = None
+
+# handle SIGTERM (i.e. kill PID)
+import signal
+def signal_term_handler(signal, frame):
+    print("signal_term_handler: {0}, {1}".format(signal, frame))
+
+    # stop scroller & clear LCD
+    if lcdPluginInstance != None:
+        lcdPluginInstance.stopScrollers()
+
+    # stop gps controller
+    if gpsPluginInstance != None:
+        gpsPluginInstance.stopController()
+
+    # Shutdown the logging system
+    logging.shutdown()
+
+    sys.exit(1)
+
 def get_subclasses(mod, cls):
     for name, obj in inspect.getmembers(mod):
         if hasattr(obj, "__bases__") and cls in obj.__bases__:
@@ -48,7 +70,6 @@ class MissingField(Exception): pass
 
 # Inputs
 sensorPlugins = []
-gpsPluginInstance = None
 def getInputs():
 
     global gpsPluginInstance # required as updated here
@@ -127,7 +148,6 @@ def getInputs():
 
 # Outputs
 outputPlugins = []
-lcdPluginInstance = None
 def getOutputs():
 
     outputConfig = ConfigParser.SafeConfigParser()
@@ -388,6 +408,9 @@ def runAirPi():
             gpsPluginInstance.stopController()
 
 def main():
+
+    # handle process being killed
+    signal.signal(signal.SIGTERM, signal_term_handler)
 
     try:
         try:
