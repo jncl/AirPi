fb59f0223601f9333c6778637091fd44b187fa24
Jncl <jncl@mac.com>
2014-06-18 23:58:02 +0100

    Updated GPS info display string again

--- outputs/lcdpanel.py
+++ outputs/lcdpanel.py
@@ -60,7 +60,7 @@ class LCDpanel(output.Output):
                 # handle GPS data when available
                 if i["type"] == "Location":
                     if i["lat"] > 0.0:
-                        disp_str = u"DT: {0} {1}, Posn: {2:.3f}{3} {4:.3f}{5} Alt: {6} m ".format(i["utc"][:10], i["utc"][11:18], i["lat"], ds, i["lon"], ds, i["ele"])
+                        disp_str = u"DT: {0} {1}, Posn: {2:.3f}{3} {4:.3f}{5} Alt: {6} m ".format(i["utc"][:10], i["utc"][11:19], i["lat"], ds, i["lon"], ds, i["ele"])
                     else:
                         disp_str = u"DT: Unknown Posn: Unknown "
                 elif i["type"] == "Temperature":
